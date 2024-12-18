ab0ade70f56b40043c78ee95e897eb4944c474e9
kinqbert <perehodko1010@gmail.com>
2024-12-18 21:41:25 +0200

    Update UpdateUserController to set default avatar URL; remove avatarUrl from validation

--- src/controllers/user/UpdateUserController.ts
+++ src/controllers/user/UpdateUserController.ts
@@ -7,19 +7,23 @@ export const UpdateUserController: RequestHandler = async (req, res) => {
   const userId = (req as UserRequest).userId;
   const { name, surname, licenseNumber, avatarUrl } = req.body;
 
-  if (!name || !surname || !licenseNumber || !avatarUrl) {
+  if (!name || !surname || !licenseNumber) {
     ResponseService.error(res, "Validation error", 400);
     return;
   }
 
+  const userAvatar =
+    avatarUrl ||
+    "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541";
+
   await UserModel.update(
-    { name, surname, licenseNumber, avatarUrl },
+    { name, surname, licenseNumber, avatarUrl: userAvatar },
     { where: { id: userId } }
   );
 
   ResponseService.success(
     res,
-    { name, surname, licenseNumber, avatarUrl },
+    { name, surname, licenseNumber, avatarUrl: userAvatar },
     200
   );
 };
